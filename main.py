11954b88cfdcc64df17ff4de6a2d4cc26e5c9408
IgorTatarinov <ig.tatarinov@gmail.com>
2022-10-01 14:28:26 +0300

    update to only main

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="static")
 
+
 @app.route('/')
 def hello():
     return render_template("index.html")
+
 
 @app.route('/map')
 def map():
     return render_template("map.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
