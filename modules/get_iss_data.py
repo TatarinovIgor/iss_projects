d857afcf5934af82f5e7ad68f45d360017e2d235
Nnnnest <77228806+Nnnnest@users.noreply.github.com>
2022-10-01 13:55:42 +0300

    Altitude
    Added altitude of the iss

--- modules/get_iss_data.py
+++ modules/get_iss_data.py
@@ -5,6 +5,7 @@ import time
 import webbrowser
 import geocoder
 
+#get the ampunt of astronauts on the iss
 url = "http://api.open-notify.org/astros.json"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
@@ -15,13 +16,9 @@ file.write("There are currently " +
 people = result["people"]
 for p in people:
     file.write(p['name'] + " - on board" + "\n")
-# print long and lat
-g = geocoder.ip('me')
-file.write("\nYour current lat / long is: " + str(g.latlng))
 file.close()
-webbrowser.open("iss.txt")
 
-# Setup the world map in turtle module
+
 screen = turtle.Screen()
 screen.setup(1280, 720)
 screen.setworldcoordinates(-180, -90, 180, 90)
@@ -36,20 +33,21 @@ iss.penup()
 
 while True:
     # load the current status of the ISS in real-time
-    url = "http://api.open-notify.org/iss-now.json"
+    url = "https://api.wheretheiss.at/v1/satellites/25544"
     response = urllib.request.urlopen(url)
     result = json.loads(response.read())
 
     # Extract the ISS location
-    location = result["iss_position"]
-    lat = location['latitude']
-    lon = location['longitude']
+    lat = result['latitude']
+    lon = result['longitude']
+    alt = result['altitude']
 
     # Ouput lon and lat to the terminal
     lat = float(lat)
     lon = float(lon)
     print("\nLatitude: " + str(lat))
     print("\nLongitude: " + str(lon))
+    print("\nAltitude: " + str(alt))
 
     # Update the ISS location on the map
     iss.goto(lon, lat)
