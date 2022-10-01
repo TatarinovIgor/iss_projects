1f5c1ea77bf215dca553682075ae975b5309ba9d
IgorTatarinov <ig.tatarinov@gmail.com>
2022-10-01 13:23:14 +0300

    map added

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__, static_folder="static")
 def hello():
     return render_template("index.html")
 
+@app.route('/map')
+def map():
+    return render_template("map.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
