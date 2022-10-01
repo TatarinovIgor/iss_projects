d2b12441488b715e9b746255b81a5b1c2714daef
Nnnnest <77228806+Nnnnest@users.noreply.github.com>
2022-10-01 13:17:59 +0300

    basic code
    basic code to show amount of astronauts and the location of iss

--- modules/get_data.py
+++ modules/get_data.py
@@ -8,6 +8,7 @@ import geocoder
 url = "http://api.open-notify.org/astros.json"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
+
 file = open("iss.txt", "w")
 file.write("There are currently " +
            str(result["number"]) + " astronauts on the ISS: \n\n")
