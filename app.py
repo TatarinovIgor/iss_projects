404d0f37345157407b66f3611805c0d1eedc43e7
IgorTatarinov <ig.tatarinov@gmail.com>
2022-10-02 14:27:20 +0300

    2d tracker data added

--- app.py
+++ app.py
@@ -1,12 +1,60 @@
+import json
+import urllib.request
 from distutils.debug import DEBUG
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="static")
 
+metric_imperial = False
+
+
+def position():
+    url = "https://api.wheretheiss.at/v1/satellites/25544"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+
+    if metric_imperial:
+        lat = result['latitude']
+        lon = result['longitude']
+        alt = result['altitude'] * 0.62137119
+        vel = result['velocity'] * 0.62137119
+    else:
+        lat = result['latitude']
+        lon = result['longitude']
+        alt = result['altitude']
+        vel = result['velocity']
+    return lat, lon, alt, vel
+
+
+if metric_imperial:
+    system = "miles"
+    altitude_unit = '"miles"'
+    velocity_unit = '"miles/hours"'
+else:
+    system = "kilometers"
+    velocity_unit = '"kilometers/hours"'
+    altitude_unit = '"kilometers"'
+
 
 @app.route('/')
-def hello():
-    return render_template("index.html")
+def output():
+    # get the ampunt of astronauts on the iss
+    url = "http://api.open-notify.org/astros.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+    people = result["people"]
+    counter = 0
+    text = '{"people" : ['
+    for p in people:
+        if p["craft"] == "ISS":
+            counter += 1
+            text += '{"name" : ' + '"' + p['name'] + '"' + '},'
+
+    lat, lon, alt, vel = position()
+
+    data = {'lat': lat, 'lon': lon, 'alt': alt, 'vel': vel}
+
+    return render_template("tracker2d.html", data=data)
 
 
 @app.route('/map')
